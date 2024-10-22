f60e843d403169ccd5bf73fc1df340b67e06c80d
Khumoyun Khusanov <khusanov.x.05@gmail.com>
2024-10-22 22:05:15 +0500

    Add password input and button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,36 +11,76 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.cyanAccent,
       appBar: AppBar(
         backgroundColor: Colors.orange,
         title: Text('Homework'),
       ),
 
-      body: Center(
-        child: Column(
+      body: Container(
+        width: double.infinity,
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
 
-          children: [
-
-            Container(
-              height: 30,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius(15),
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Enter your email address',
-                  hintStyle: TextStyle(color: Colors.green),
-                  border: InputBorder.none,
-                  prefixIcon: Icon(
-                    Icons.email,
-                    color: Colors.orange,
-                  )
+              Container(
+                height: 50,
+                width: 300,
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
                 ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter your email address',
+                      hintStyle: TextStyle(color: Colors.green),
+                      border: InputBorder.none,
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.orange,
+                      )
+                    ),
+                  ),
               ),
-            ),
+              Container(
+                height: 50,
+                width: 300,
+                padding: EdgeInsets.all(15),
+                margin: EdgeInsets.only(top: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Enter your password',
+                        hintStyle: TextStyle(color: Colors.green),
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.password,
+                          color: Colors.orange,
+                        )
+                    ),
+                  ),
+              ),
 
-          ],
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                width: 300,
+                height: 50,
+                child: MaterialButton(
+                  color: Colors.orange,
+                  onPressed: () {},
+                  child: Text('Sign Up', style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                ),
+              )
 
+            ],
+
+          ),
         ),
       ),
     );
